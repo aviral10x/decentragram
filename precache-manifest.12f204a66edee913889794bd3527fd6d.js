abf1c584ca321652c076d06a9552fa4f5ae510a8
Aviral <21f1000608@student.onlinedegree.iitm.ac.in>
2022-03-03 00:40:51 +0530

    Updates

---
+++ precache-manifest.12f204a66edee913889794bd3527fd6d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b05b2939adf3b149dbca",
+    "url": "/decentragram/static/css/main.88ea2865.chunk.css"
+  },
+  {
+    "revision": "b05b2939adf3b149dbca",
+    "url": "/decentragram/static/js/main.b05b2939.chunk.js"
+  },
+  {
+    "revision": "fcc8d16d85d5475b407d",
+    "url": "/decentragram/static/css/1.23fd5867.chunk.css"
+  },
+  {
+    "revision": "fcc8d16d85d5475b407d",
+    "url": "/decentragram/static/js/1.fcc8d16d.chunk.js"
+  },
+  {
+    "revision": "a4a45a276db0bfa179c8",
+    "url": "/decentragram/static/js/runtime~main.a4a45a27.js"
+  },
+  {
+    "revision": "d6cbd8e6d17d679dd3cd7fe3884ac8bb",
+    "url": "/decentragram/static/media/photo.d6cbd8e6.png"
+  },
+  {
+    "revision": "892d94c6bc6771c2e2004a480c464f3b",
+    "url": "/decentragram/index.html"
+  }
+];
